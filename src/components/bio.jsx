886b363adb4b25a88da4edc772a888959b55c1a1
Hamish Hall <hachall@hotmail.com>
2019-10-20 15:51:31 +0100

    experience added to bio

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -11,13 +11,31 @@ class Bio extends Component {
           <Link to='/' className="white-link bio-about-link">
             SHINYA UCHIDA
           </Link>
-          <div className="nav-name">
-            ABOUT
-          </div>
-          <div className="bio-info">
-            <p>Tokyo Born. London Based.</p>
-            <p>Japanese-English Hybrid.</p>
-            <p>Graphic Designer.</p>
+          <div className="bio-wrapper">
+            <div>
+              <div className="nav-name">
+                ABOUT
+              </div>
+              <div className="bio-info">
+                <div>
+                  <p>Tokyo Born. London Based.</p>
+                  <p>Japanese-English Hybrid.</p>
+                  <p>Graphic Designer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="left-margin">
+              <div className="nav-name">
+                EXPERIENCE
+              </div>
+              <div className="bio-info">
+                <div>
+                  <p><a className = "borderCenter" href="https://www.paq.works/">paq</a></p>
+                  <p><a className = "borderCenter" href="https://utile.studio/">UTILE</a></p>
+                  <p><a className = "borderCenter" href="http://www.minititle.com/">Mini Title</a></p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="bio-bottom">
