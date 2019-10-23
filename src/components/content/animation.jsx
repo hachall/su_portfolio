13ed57c405e9976c33ab6d6723520b84313c9d3b
Hamish Hall <hachall@hotmail.com>
2019-10-23 16:26:46 +0100

    gif updated

--- src/components/content/animation.jsx
+++ src/components/content/animation.jsx
@@ -1,6 +1,6 @@
 const animation = [
   {
-    src: "https://shinyaportfolioeu.s3.eu-west-2.amazonaws.com/new_images/Website+Images/7.+Personal+work/Rainbow+Distort.gif",
+    src: "https://shinyaportfolioeu.s3.eu-west-2.amazonaws.com/new_images/Website+Images/7.+Personal+work/Rainbow+Distort_03.gif",
     img: true
   }
 ];
